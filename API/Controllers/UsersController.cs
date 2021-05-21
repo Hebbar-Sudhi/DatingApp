0c4bfb6997a00c92b43c8e636eb60ef8a1142eb6
Sudheendra HEBBAR <sudheendra.hebbar@alstomgroup.com>
2021-05-21 18:20:06 +0530

    End of Chapter 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        // Constructor to initialize the data context
+        public UsersController(DataContext context)
+        {
+            _context = context;
+
+            foreach(var user in _context.Users.ToList())
+            {
+                Console.WriteLine(user.UserName);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
